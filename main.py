85ab76904adbaa3eea5a7297957a0cc12fcfb8ac
Alena <vaskova.a.n.2002@mail.ru>
2022-11-29 13:36:49 +0400

    Добавлен обработчик действий пользователя

--- main.py
+++ main.py
@@ -17,3 +17,30 @@ def showMenu():
     print('\n'.join([f'{key}: {value}' for key, value in menu.items()]))
 
 showMenu()
+
+while True:
+    command = input()
+    if command == '1':
+        print(1)
+    elif command == '2':
+        print(2)
+    elif command == '3':
+        print(3)
+    elif command == '4':
+        print(4)
+    elif command == '5':
+        print(5)
+    elif command == '6':
+        print(6)
+    elif command == '7':
+        print(7)
+    elif command == '8':
+        print(8)
+    elif command == '9':
+        print(9)
+    elif command == '0':
+        showMenu()
+    elif command == 'exit':
+        break
+    else:
+        print('Неверная команда, попробуйте ещё раз', '0: Назад в меню', sep='\n')
